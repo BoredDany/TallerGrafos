13b7273d3810f48e11cb8e7e0c13c99be561efea
Santag207 <castrosantiago476@gmail.com>
2023-11-14 19:49:13 -0500

    Commit

--- Grafo.h
+++ Grafo.h
@@ -19,7 +19,32 @@ private:
     std::vector < std::list < std::pair < int, C > > > edges;
 
 public:
+    //constructors
+    Graph();
 
+    //getters
+    std::vector < Punto > getVertices();
+    std::vector < std::list < std::pair < int, C > > > getEdges();
+
+    //setters
+    void setVertices(std::vector < Punto >& vertices);
+    void setEdges(std::vector < std::list < std::pair < int, C > > >& edges);
+
+    //inserting
+    bool addVertex(Punto& vertex);
+    bool addEdge(Punto& origin, Punto& destination, C cost);
+
+    //searching
+    int searchVertice(Punto& vertex);
+    bool searchEdge(Punto& origin, Punto& destination);
+
+    //tours
+    void plain();
+    void showEdges();
+
+    //algorithms
+    void bestRoute(Punto& initial, std::vector <int>& route, C& distance);
+    void findBestRoute(int indexBegin, std::vector<bool> & visited, std::vector <int>& route, C& distance);
 
 };
 
